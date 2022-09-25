4cec9167b4ea307f285461df3e0c803ae697e3b0
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-09-25 18:03:49 +0700

    Enhance search activity

--- app/src/main/java/com/robert/movieapp/view/search/SearchActivity.kt
+++ app/src/main/java/com/robert/movieapp/view/search/SearchActivity.kt
@@ -57,11 +57,11 @@ class SearchActivity : AppCompatActivity() {
         })
 
         searchItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
                 return true
             }
 
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
                 onBackPressed()
                 return false
             }
